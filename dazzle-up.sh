ec17d560418319d04d87e88d808df7d9ad7a0193
Andrei Jiroh Eugenio Halili <ajhalili2006@gmail.com>
2022-02-04 16:58:30 +0800

    chore(dazzle): update dazzle-up script to support overriding REPO var
    Signed-off-by: Andrei Jiroh Eugenio Halili <ajhalili2006@gmail.com>

--- dazzle-up.sh
+++ dazzle-up.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-REPO=localhost:5000/dazzle
+REPO=${IMAGE_ARTIFACTS_REPO:-"localhost:5000/dazzle"}
 # First, build chunks without hashes
 dazzle build $REPO -v --chunked-without-hash
 # Second, build again, but with hashes
