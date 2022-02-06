de314b0d304f4583077163d88e3276c8ec34d4a1
Andrei Jiroh Eugenio Halili <ajhalili2006@gmail.com>
2022-02-06 07:22:58 +0000

    chore(gitlab-cicd): 👷 update CI jobs to make Dazzle builds work + some updates on devkit scripts
    We changed the default repo path for our dazzle-up script on root by the way,
    but also introduced Skopeo for synchorizing stuff.
    It's still an work in progress through, so we'll probably
    go into that later. I also run pre-commit for the linting stuff.
    
    Signed-off-by: Andrei Jiroh Eugenio Halili <ajhalili2006@gmail.com>

--- dazzle-up.sh
+++ dazzle-up.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-REPO=${IMAGE_ARTIFACTS_REPO:-"localhost:5000/dazzle"}
+REPO=${IMAGE_ARTIFACTS_REPO:-"localhost:5000/recaptime-dev/gp-ws-images-build-artifacts"}
 # First, build chunks without hashes
 dazzle build $REPO -v --chunked-without-hash
 # Second, build again, but with hashes
