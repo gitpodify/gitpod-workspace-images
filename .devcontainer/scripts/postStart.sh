7a8658964b6edea806706f65794282d409d133f5
Recap Time Squad <yourfriends@recaptime.tk>
2022-05-19 09:04:37 +0000

    chore(gitpod): 🧑‍💻 update setup scripts for both Gitpod and GitHub Codespaces
    Signed-off-by: Recap Time Squad <yourfriends@recaptime.tk>

--- .devcontainer/scripts/postStart.sh
+++ .devcontainer/scripts/postStart.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
-direnv allow
-echo
+direnv allow # allow our .envrc to be executed
 
+nohup ./devkit/gp-buildkit >> /dev/null & # start Buildkitd with our repo-specific configs
+
+# Authenticate Docker CLI and Skopeo
+./devkit/docker-cli-login || true
+./devkit/bin/skopeo-auth-ghcr || true
+
+# Start local registry and show welcome message
 ./devkit/local-registry
